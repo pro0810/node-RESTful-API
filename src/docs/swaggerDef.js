edd8398cb00ddb57e325606ddc9b011320a4fb65
suhaa18 <suhaa18@zoho.com>
2022-08-09 20:57:03 -0400

    update swagger title

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -4,11 +4,10 @@ const config = require('../config/config');
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-boilerplate API documentation',
+    title: 'bpx backend API documentation',
     version,
     license: {
       name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
     },
   },
   servers: [
